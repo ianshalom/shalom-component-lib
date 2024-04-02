dd28ecd14bf5880be1c81ebe75e4155ee502a2ad
Ian <ian@bambu.co>
2024-04-02 14:35:35 +0800

    v1.0.27: add jsxRuntime classic file

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 /// <reference types="vitest" />
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react({ jsxRuntime: "classic" })],
   resolve: {
     alias: {
       "@": "/src",
